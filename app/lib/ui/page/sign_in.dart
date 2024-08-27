28e9ed24933e07cb0be98a5bf99f0430b9981a82
Zp1k_e <miche.atucha@gmail.com>
2024-08-27 15:21:58 -0500

    SignIn page username

--- app/lib/ui/page/sign_in.dart
+++ app/lib/ui/page/sign_in.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
 
   @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final usernameCtrl = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('SIGN IN TODO'),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              minWidth: 200,
+              maxWidth: 400,
+            ),
+            child: body(context),
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget body(context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      children: [
+        usernameField(context),
+      ],
+    );
+  }
+
+  Widget usernameField(context) {
+    return TextFormField(
+      controller: usernameCtrl,
+      autofocus: true,
+      autocorrect: false,
+      decoration: InputDecoration(
+        labelText: L10n.of(context).username,
+        icon: const Icon(Icons.abc_sharp),
+      ),
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (value == null || value.length < 2) {
+          return L10n.of(context).invalidSignInUsername;
+        }
+        return null;
+      },
     );
   }
 }
