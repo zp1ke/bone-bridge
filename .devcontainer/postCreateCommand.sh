4960c5390fb43a8d56b9878b33c1c64689b2092a
Sp1k_e <miche.atucha@gmail.com>
2024-09-08 10:58:47 -0500

    appwrite project

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,12 @@ echo ""
 flutter --version
 
 git config --global --add safe.directory /workspaces/*
+
+echo ""
+if [ $(dpkg-query -W -f='${Status}' appwrite 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "Appwrite CLI not found. Installing...";
+  curl -sL https://appwrite.io/cli/install.sh | bash;
+  appwrite client --endpoint https://cloud.appwrite.io/v1;
+fi
+appwrite -v
