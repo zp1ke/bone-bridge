863ceda7fb2ee7953d1030e2a686a2035b512738
Michel Atucha <zp1ke@outlook.com>
2025-02-05 18:24:56 -0500

    test org repo

--- back/app/src/test/java/org/bone/bridge/back/app/TestcontainersConfig.java
+++ back/app/src/test/java/org/bone/bridge/back/app/TestcontainersConfig.java
@@ -3,14 +3,27 @@ package org.bone.bridge.back.app;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfig {
+    @Container
+    public final static PostgreSQLContainer<?> container = new PostgreSQLContainer<>(DockerImageName.parse("postgres"));
+
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+    }
+
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return container;
     }
 }
---
+++ back/app/src/test/java/org/bone/bridge/back/app/repo/OrganizationRepoTests.java
@@ -0,0 +1,60 @@
+package org.bone.bridge.back.app.repo;
+
+import java.time.OffsetDateTime;
+import org.bone.bridge.back.app.TestcontainersConfig;
+import org.bone.bridge.back.app.domain.Organization;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+@Import(TestcontainersConfig.class)
+public class OrganizationRepoTests {
+    @Autowired
+    OrganizationRepo organizationRepo;
+
+    @Test
+    public void testSaveOrganization() {
+        var data = Organization.builder()
+            .code("TEST")
+            .name("Test Organization")
+            .build();
+        var organization = organizationRepo.save(data);
+        assertNotNull(organization.getId());
+        assertEquals(data.getCode(), organization.getCode());
+        assertEquals(data.getName(), organization.getName());
+    }
+
+    @Test
+    public void testFindOrganizationsCreatedBetween() {
+        var organization = organizationRepo.save(Organization.builder()
+            .code("TEST")
+            .name("Test Organization")
+            .build());
+
+        var now = OffsetDateTime.now();
+        var spec = OrganizationSpec.createdBetween(now.minusMinutes(1L), now);
+        var organizations = organizationRepo.findAll(spec);
+        assertNotNull(organizations);
+        assertFalse(organizations.isEmpty());
+        assertEquals(1, organizations.size());
+        assertEquals(organization.getCode(), organizations.getFirst().getCode());
+        assertEquals(organization.getName(), organizations.getFirst().getName());
+
+        spec = OrganizationSpec.createdBetween(now, now.plusMinutes(1L));
+        organizations = organizationRepo.findAll(spec);
+        assertNotNull(organizations);
+        assertTrue(organizations.isEmpty());
+
+        spec = OrganizationSpec.createdBetween(now.minusMinutes(2L), now.minusMinutes(1L));
+        organizations = organizationRepo.findAll(spec);
+        assertNotNull(organizations);
+        assertTrue(organizations.isEmpty());
+    }
+}
